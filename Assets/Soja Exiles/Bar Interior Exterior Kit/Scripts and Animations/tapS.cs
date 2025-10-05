f27e27a1073d95f5ed30a38f21844842a94d0cf7
minnnpannn9585 <panmin677095@gmail.com>
2025-10-05 11:31:50 +0800

    add bottle assets

---
+++ Assets/Soja Exiles/Bar Interior Exterior Kit/Scripts and Animations/tapS.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tapS: MonoBehaviour {
+
+	public Animator TapPour;
+	public Transform Player;
+    public bool pouring;
+
+    void Start()
+    {
+        pouring = false;
+    }
+
+    void OnMouseOver (){
+		{
+			if (Player) {
+				float dist = Vector3.Distance (Player.position, transform.position);
+				if (dist < 15) {
+                    if (pouring == false)
+                    {
+                        if (Input.GetMouseButtonDown(0))
+                        {
+                            TapPour.Play("Pour");
+                            pouring = true;
+                            print("you are pouring");
+                        }
+                       
+                    }
+                }
+			}
+
+		}
+
+	}
+
+    void OnMouseExit()
+    {
+        {
+            if (pouring == true)
+            {
+                TapPour.Play("Pour1");
+                pouring = false;
+            }
+        }
+    }
+
+}
